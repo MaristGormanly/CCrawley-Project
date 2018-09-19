8cdbd970a69ccc75a76318ff4b2071e31a2f978d
Chris <guitar12698@hotmail.com>
2018-09-19 14:56:01 -0400

    1.3

--- classdemo112/server/oregonTrail.js
+++ classdemo112/server/oregonTrail.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-
-app.use(express.static('clinet/public'))
+app.use(express.static('client/public'))
 app.get('/', function(request, response){
   response.sendFile('index.html', {root: './client/views'})
 })
+
 
 app.listen(1337,() => console.log("this is working"));
