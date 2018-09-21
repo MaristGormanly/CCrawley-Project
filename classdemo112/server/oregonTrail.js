41720a3206f32191ff907225592244d64d83aa02
Chris <guitar12698@hotmail.com>
2018-09-21 10:40:42 -0400

    1.5

--- classdemo112/server/oregonTrail.js
+++ classdemo112/server/oregonTrail.js
@@ -2,9 +2,25 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('client/public'))
+
 app.get('/', function(request, response){
-  response.sendFile('index.html', {root: './client/views'})
+  response.sendFile('index', {root: './client/views'})
 })
 
+app.get('/mainmenu', function(request,response){
+  response.sendFile('mainmenu', {root: './client/views'})
+})
+
+app.get('/setup', function(request,response){
+  response.sendFile('setup', {root: './client/views'})
+})
+
+app.get('/topten', function(request,response){
+  response.sendFile('topten', {root: './client/views'})
+})
+
+app.get('/trail', function(request,response){
+  response.sendFile('trail', {root: './client/views'})
+})
 
 app.listen(1337,() => console.log("this is working"));
